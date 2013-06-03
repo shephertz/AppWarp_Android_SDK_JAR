386f1119b248c9ed7da3254133a6d920cf81a3ab
DhruvCShepHertz <dhruv.chopra@shephertz.co.in>
2013-06-03 19:57:08 +0530

    Update Constants.java

--- MatchMakingSample/src/com/example/matchmaking/Constants.java
+++ MatchMakingSample/src/com/example/matchmaking/Constants.java
@@ -2,8 +2,10 @@ package com.example.matchmaking;
 
 public class Constants {
 
-    public static String apiKey = "1adc4a4cf9563445d1326da3485f74ec7e9286542f1f3c6b36691d100fb3c592";
-    public static String secretKey =  "f11b0f2a47a699c7e5c5677a4807c53dbb98a60a08ba9598b74adf64a3616557";
+    // Enter the values for keys you received after creating an AppWarp app from
+    // AppHQ developer console
+    public static String apiKey = "Your API Key Here";
+    public static String secretKey =  "Your Secret Key Here";
 
 
 }
